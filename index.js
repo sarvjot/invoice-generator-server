1ce56084298e28ce1e60cd1731d88d01878138fe
sarvjot <sarvjotsingh422002@gmail.com>
2023-04-10 21:38:36 +0530

    host 0.3

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
-
 import { itemRouter, billRouter } from './routes/index.js';
 
-const mongodbURI = process.env.MONGODB_URI;
+dotenv.config();
 
 mongoose
-    .connect(mongodbURI, { useNewUrlParser: true })
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
     .then(() => console.log("Database Connected Successfully"))
     .catch((err) => console.log(err));
 
